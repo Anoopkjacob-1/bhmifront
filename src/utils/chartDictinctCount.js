520efad2dda1eb751ccfa826f867571002a1e549
Anoopkjacob-1 <anoopkjacob2021@mca.ajce.in>
2021-08-02 00:25:56 +0530

    new build

--- src/utils/chartDictinctCount.js
+++ src/utils/chartDictinctCount.js
@@ -1,7 +1,13 @@
 
 
 const DistinctCount=(Data)=>{
-let count=Data.reduce( (acc, o) => (acc[o.bmitext] = (acc[o.bmitext] || 0)+1, acc),{} );
+let count=Data.map(({ bmitext }) => bmitext)
+.reduce((names, bmitext) => {
+  const count = names[bmitext] || 0;
+  names[bmitext] = count + 1;
+  return names;
+}, {});
+  console.log(count)
     let result=Object.values(count)
     return result
 }
